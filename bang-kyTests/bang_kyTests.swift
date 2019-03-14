fa07b3c56f23e00724555d9df3db2cd144c9b10c
Sourav Ganguly <sourav.ganguly@bjitgroup.com>
2019-03-14 12:11:19 +0600

    Created Project

---
+++ bang-kyTests/bang_kyTests.swift
@@ -0,0 +1,34 @@
+//
+//  bang_kyTests.swift
+//  bang-kyTests
+//
+//  Created by Saurav Ganguly-402 on 14/3/19.
+//  Copyright © 2019 bjitgroup. All rights reserved.
+//
+
+import XCTest
+@testable import bang_ky
+
+class bang_kyTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
